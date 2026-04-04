dae1dfc19d082b1a028a90c0b1c2fe6686857587
deelicatesse <autoerotionasphyxiation@gmail.com>
2026-04-04 16:39:02 -0600

    migrate nvim-treesitter config to branch=main,update for Nvim 0.12 sáb 04 abr 2026 16:39:02 CST

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -1,3 +1,4 @@
+--[[
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
@@ -10,4 +11,31 @@ return {
       incremental_selection = { enable = true },
     })
   end,
+}
+--]]
+-- UPDATE TO VERSION 0.12.0
+-- ENSURE TO INSTALL treesitter-cli via npm 
+return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main", --  <------ agrega esto
+    build = ":TSUpdate",
+    opts = {
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
+    -- remove if not works
+   init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                -- Enable treesitter highlighting and disable regex syntax
+                pcall(vim.treesitter.start)
+
+                -- Enable treesitter-based indentation
+                vim.bo.indentexpr =
+                    "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
+
 }
